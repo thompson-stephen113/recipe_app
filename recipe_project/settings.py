5928ca63a0d5ea1fa174c717567aa492918a414a
Stephen Thompson <thompson.stephen113@gmail.com>
2024-07-10 22:04:16 -0400

    modified:   recipe_project/settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Static file storage with WhiteNoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
